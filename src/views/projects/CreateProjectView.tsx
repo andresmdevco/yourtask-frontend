18c5f73a6b20f4a721e64c716e56370eaeb34932
Andrés Muñoz <andres2078@gmail.com>
2026-09-22 17:44:32 -0500

    feat: handle backend errors in project mutations

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -21,8 +21,8 @@ export default function CreateProjectView() {
 
   const { mutate } = useMutation({
     mutationFn: createProject,
-    onError: () => {
-
+    onError: (error) => {
+      toast.error(error.message)
     },
     onSuccess: (data) => {
       toast.success(data);
