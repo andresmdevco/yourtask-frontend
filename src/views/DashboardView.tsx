8b4e11122623a70ce075fd3ec6c0b4f843d9fc11
Andrés Muñoz <andres2078@gmail.com>
2026-09-22 21:30:18 -0500

    feat: validate API responses with Zod

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -11,19 +11,31 @@ export default function DashboardView() {
   if (isLoading) return 'Cargando...';
   console.log(data);
 
-  return (
-    <>
-      <h1 className="text-5xl font-black">Mis Proyectos</h1>
-      <p className="text-2xl font-light text-gray-500 mt-5">Maneja y administra tus proyectos</p>
+  if (data)
+    return (
+      <>
+        <h1 className="text-5xl font-black">Mis Proyectos</h1>
+        <p className="text-2xl font-light text-gray-500 mt-5">Maneja y administra tus proyectos</p>
 
-      <nav className="my-5">
-        <Link
-          className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
-          to="/projects/create"
-        >
-          Nuevo Proyecto
-        </Link>
-      </nav>
-    </>
-  );
+        <nav className="my-5">
+          <Link
+            className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+            to="/projects/create"
+          >
+            Nuevo Proyecto
+          </Link>
+        </nav>
+
+        {data.length ? (
+          <p>Si hay proyectos</p>
+        ) : (
+          <p className="text-center py-20">
+            No hay proyectos aún {''}
+            <Link to="/projects/create" className="text-fuchsia-500 font-bold">
+              Crear Proyecto
+            </Link>
+          </p>
+        )}
+      </>
+    );
 }
