63192338959c5d98104b3a647caff907ffda900c
Andrés Muñoz <andres2078@gmail.com>
2026-09-23 18:08:33 -0500

    feat: create task form

--- src/components/tasks/AddTaskModal.tsx
+++ src/components/tasks/AddTaskModal.tsx
@@ -1,6 +1,9 @@
 import { Fragment } from 'react';
 import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from '@headlessui/react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import TaskForm from './TaskForm';
+import type { TaskFormData } from '@/types/index';
 
 export default function AddTaskModal() {
   const navigate = useNavigate();
@@ -8,6 +11,21 @@ export default function AddTaskModal() {
   const queryParams = new URLSearchParams(location.search);
   const modalTask = queryParams.get('newTask');
   const show = modalTask ? true : false;
+
+  const initialValues: TaskFormData = {
+    name: '',
+    description: '',
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: initialValues });
+
+  const handleCreateTask = (formData: TaskFormData) => {
+    console.log(formData);
+  };
 
   return (
     <>
@@ -49,6 +67,19 @@ export default function AddTaskModal() {
                     Llena el formulario y crea {''}
                     <span className="text-fuchsia-600">una tarea</span>
                   </p>
+
+                  <form
+                    className="mt-10 space-y-3"
+                    noValidate
+                    onSubmit={handleSubmit(handleCreateTask)}
+                  >
+                    <TaskForm register={register} errors={errors} />
+                    <input
+                      type="submit"
+                      value="Guardar Tarea"
+                      className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3 text-white uppercase font-bold cursor-pointer transition-colors"
+                    />
+                  </form>
                 </DialogPanel>
               </TransitionChild>
             </div>
