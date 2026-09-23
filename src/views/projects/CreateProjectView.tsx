2eef8818a9b9af2bcb0e78bcd094ff4bb49d6e70
Andrés Muñoz <andres2078@gmail.com>
2026-09-23 13:11:35 -0500

    feat: handle project update mutation feedback

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -22,7 +22,7 @@ export default function CreateProjectView() {
   const { mutate } = useMutation({
     mutationFn: createProject,
     onError: (error) => {
-      toast.error(error.message)
+      toast.error(error.message);
     },
     onSuccess: (data) => {
       toast.success(data);
