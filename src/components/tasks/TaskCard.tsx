90d12eb582a6f347617332b9b3982d981900914a
Andrés Muñoz <andres2078@gmail.com>
2026-09-25 20:18:54 -0500

    feat: group tasks by status

---
+++ src/components/tasks/TaskCard.tsx
@@ -0,0 +1,11 @@
+import type { Task } from "@/types/index";
+
+type TaskCardProps = {
+  task: Task;
+}
+
+export default function TaskCard({ task }: TaskCardProps) {
+  return (
+    <div>TaskCard</div>
+  )
+}
